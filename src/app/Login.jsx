e8c2104aaf5f073a2a7fdb2492e3c86cdd924747
Abdullahar990 <abdullahar990@gmail.com>
2025-04-21 01:29:26 +0500

    Your commit message describing the changes

--- src/app/Login.jsx
+++ src/app/Login.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import styles from './CSS/login.module.css'
+import Image from 'next/image';
+import React from 'react';
+import styles from './CSS/login.module.css';
 
 export default function Login() {
     return (
         <div className={styles.background}>
             <div className={styles.foreground}>
-
+                <Image src="/image.png" alt="Login Visual" width={274} height={239} className={styles.img} />
             </div>
         </div>
-    )
+    );
 }
