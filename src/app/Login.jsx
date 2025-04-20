f7ee8b9ddc6071fe6bc1182f31391e54897f4083
Abdullahar990 <abdullahar990@gmail.com>
2025-04-21 01:04:49 +0500

    Your commit message describing the changes

---
+++ src/app/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './CSS/login.module.css'
+
+export default function Login() {
+    return (
+        <div className={styles.background}>
+            <div className={styles.foreground}>
+
+            </div>
+        </div>
+    )
+}
